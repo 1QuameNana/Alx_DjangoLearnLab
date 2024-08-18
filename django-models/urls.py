398928a0d9c240196d4537d95e15a0d56c77c91b
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-08-18 14:04:10 +0100

    relationship_app/views.py

--- django-models/urls.py
+++ django-models/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', list_books, name='list_books'),  
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  
+
 ]
