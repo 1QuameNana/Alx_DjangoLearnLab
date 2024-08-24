5dd6107681d461db0e29c4137a186d687770ba6d
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-08-24 17:46:13 +0100

    relationship_appp

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', list_books, name='list_books'),  
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  
-
+    path('', include('relationship_app.urls')),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
-from .models import Book
-from django.views.generic import DetailView  
+from typing import Any
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from django.views.generic import View 
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.forms import UserCreationForm
+#from .forms import BookForm
+
+from .models import Book, UserProfile
 from .models import Library
 
 
+
+
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    items = Book.objects.all()
+    book_list = [f"{i.title} by {i.author}" for i in items]
+    return render(request, "relationship_app/list_books.html", {"book_list": book_list})
+
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/Library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['library'] = self.object
+        return context
