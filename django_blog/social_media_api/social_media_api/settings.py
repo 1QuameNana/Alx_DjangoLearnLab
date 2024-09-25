25940fa69cb0f7f6ed93edd17c5aa5831ec527ae
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-09-25 11:27:40 +0100

    social_media_api

--- django_blog/social_media_api/social_media_api/settings.py
+++ django_blog/social_media_api/social_media_api/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest-framework',
-    'accounts',
+    'accounts.apps.AccountsConfig',
+    'rest_framework',
+    'rest_framework.authtoken'
+
+    
 ]
 
 MIDDLEWARE = [
@@ -101,6 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
@@ -123,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
