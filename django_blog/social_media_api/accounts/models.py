2ef39e02f438985024ad20bb3e1af16bcf2e6d85
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-09-25 10:57:05 +0100

    social_media_api

---
+++ django_blog/social_media_api/accounts/models.py
@@ -0,0 +1,9 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    bio = models.TextField()
+    profile_picture = models.ImageField(max_length=200, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False)
+# Create your models here.
