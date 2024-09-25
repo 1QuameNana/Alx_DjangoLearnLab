3c31b828e92cee6cdda228d04e19abc8debd6ba8
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-09-25 12:15:32 +0100

    posts

--- django_blog/social_media_api/social_media_api/settings.py
+++ django_blog/social_media_api/social_media_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'posts.apps.PostsConfig',
 
     
 ]
